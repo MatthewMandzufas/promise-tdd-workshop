6d02843a7752da75e915e9b2fc113d1eb75ff43e
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-03-08 15:16:09 +0800

    RED: MyPromise › should have a then method

--- src/promise.test.js
+++ src/promise.test.js
@@ -1,7 +1,11 @@
 import MyPromise from './promise';
 
-describe('mypromise', () => {
+describe('MyPromise', () => {
     it('Should be a constructor', () => {
         expect(typeof MyPromise).toBe('function');
+    });
+    it('should have a "then" method', () => {
+        const promise = new MyPromise();
+        expect(typeof promise.then).toBe('function');
     });
 });
