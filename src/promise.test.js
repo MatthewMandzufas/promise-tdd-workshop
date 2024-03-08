5526e1dd28b9dddaa9b4581e718aafdfcd9c5142
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-03-08 16:01:40 +0800

    RED: MyPromise › should resolve with a value

--- src/promise.test.js
+++ src/promise.test.js
@@ -8,4 +8,15 @@ describe('MyPromise', () => {
         const promise = new MyPromise();
         expect(typeof promise.then).toBe('function');
     });
+    it('should resolve with a value', () => {
+        const executor = (resolve) => {
+            resolve('This has resolved');
+        };
+        const promise = new MyPromise(executor);
+        const functionToCallWhenPromiseHasBeenResolved = jest.fn();
+        promise.then(functionToCallWhenPromiseHasBeenResolved);
+        expect(functionToCallWhenPromiseHasBeenResolved).toHaveBeenCalledWith(
+            'This has resolved'
+        );
+    });
 });
