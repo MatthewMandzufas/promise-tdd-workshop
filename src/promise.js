4e190e2e756462644c9096e39c7c77bc74840d55
Matthew Mandzufas <matthew.mandzufas@journeyone.com.au>
2024-03-13 12:49:17 +0800

    GREEN: MyPromise -> should resolve with a value

--- src/promise.js
+++ src/promise.js
@@ -1,5 +1,28 @@
-function MyPromise() {
-    this.then = function () {};
+const STATE = {
+    FULFILLED: 'fulfilled',
+    REJECTED: 'rejected',
+    PENDING: 'pending',
+};
+function MyPromise(executor = () => {}) {
+    this.then = function (functionToCallWhenPromiseHasBeenResolved) {
+        if (this.state === STATE.FULFILLED) {
+            functionToCallWhenPromiseHasBeenResolved(this.result);
+        } else {
+            this.functionToCallWhenPromiseHasBeenResolved =
+                functionToCallWhenPromiseHasBeenResolved;
+        }
+    };
+    this.functionToResolveThePromise = (result) => {
+        if (this.state === STATE.PENDING) {
+            this.result = result;
+            this.state = STATE.FULFILLED;
+            if (this.functionToCallWhenPromiseHasBeenResolved) {
+                this.functionToCallWhenPromiseHasBeenResolved(result);
+            }
+        }
+    };
+    this.state = STATE.PENDING;
+    executor(this.functionToResolveThePromise);
 }
 
 export default MyPromise;
